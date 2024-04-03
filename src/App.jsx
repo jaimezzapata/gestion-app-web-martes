33076f23bac951e32b129e620bf3945a7aeaeb8c
jhonD94 <jd123am@gmail.com>
2024-04-02 21:00:15 -0500

    Se crea primer componente de login y se define captura de datos con el state

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from "./layouts/components/auth/Login";
+
+function App() {
+  return (
+    <section>
+      <Login />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/layouts/components/auth/Login.jsx
@@ -0,0 +1,26 @@
+import "./Login.css";
+import { useState } from "react";
+
+const Login = () => {
+    const [getUsername, setUsername] = useState("")
+    const [getPassword, setPassword] = useState("")
+    if(getUsername === 'Jaime' && getPassword === '1234'){
+        console.log('Credenciales correctas...')
+    }
+    return (
+    <form className="login-form">
+      <h2>Login</h2>
+      <div className="input-group">
+        <label htmlFor="username">Username</label>
+        <input onChange={(e)=>{setUsername(e.target.value)}} type="text" id="username" name="username" />
+      </div>
+      <div className="input-group">
+        <label htmlFor="password">Password</label>
+        <input onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
