84e670c04827fdd2fee678d0a703b320157035ed
jaimezzapata <zapataval2304@gmail.com>
2024-04-09 18:44:45 -0500

    Se actualiza database en Login.jsx

---
+++ src/database/database.jsx
@@ -0,0 +1,14 @@
+export let usuarios = [
+  {
+    user: "admin",
+    password: "1234",
+  },
+  {
+    user: "superAdmin",
+    password: "1234",
+  },
+  {
+    user: "invitado",
+    password: "1234",
+  },
+];
--- src/layouts/components/auth/Login.jsx
+++ src/layouts/components/auth/Login.jsx
@@ -2,23 +2,40 @@ import "./Login.css";
 import { useState } from "react";
 
 const Login = () => {
-    const [getUsername, setUsername] = useState("")
-    const [getPassword, setPassword] = useState("")
-    if(getUsername === 'Jaime' && getPassword === '1234'){
-        console.log('Credenciales correctas...')
+  const [getUsername, setUsername] = useState("");
+  const [getPassword, setPassword] = useState("");
+  function iniciarSesion() {
+    if (getUsername === "Jaime" && getPassword === "1234") {
+      console.log("Credenciales correctas...");
     }
-    return (
+  }
+  return (
     <form className="login-form">
       <h2>Login</h2>
       <div className="input-group">
         <label htmlFor="username">Username</label>
-        <input onChange={(e)=>{setUsername(e.target.value)}} type="text" id="username" name="username" />
+        <input
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          type="text"
+          id="username"
+          name="username"
+        />
       </div>
       <div className="input-group">
         <label htmlFor="password">Password</label>
-        <input onChange={(e)=>{setPassword(e.target.value)}} type="password" id="password" />
+        <input
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          type="password"
+          id="password"
+        />
       </div>
-      <button type="submit">Login</button>
+      <button onClick={iniciarSesion} type="button">
+        Login
+      </button>
     </form>
   );
 };
